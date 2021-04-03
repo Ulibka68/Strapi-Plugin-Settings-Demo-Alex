245eca69d11add8a177bcda8375fc5d1ec13923c
Ulibka68 <gena68@inbox.ru>
2021-04-03 12:41:04 +0300

    вывел меню слева и в settings

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Strapi application
+# Settings API Demo App
 
-A quick description of your strapi application
+https://github.com/GalloDaSballo/Strapi-Plugin-Settings-Demo  
+
+https://www.udemy.com/course/the-complete-strapi-course/learn/lecture/21563628#questions
+
+
+### Установка 
+yarn strapi generate:plugin settings-demo
+
+### Корректировка
+plugins/settings-demo/admin/src/index.js
+
+Добвлена секция  settings:
+
+links - список элементов в подсекции
--- plugins/settings-demo/admin/src/index.js
+++ plugins/settings-demo/admin/src/index.js
@@ -12,33 +12,53 @@ export default strapi => {
   const name = pluginPkg.strapi.name;
 
   const plugin = {
+    // Props can be either null or React node (e.g. () => <div />)
     blockerComponent: null,
+    // Props to provide to customise the blockerComponent
     blockerComponentProps: {},
     description: pluginDescription,
-    icon,
     id: pluginId,
+    // type : node
     initializer: Initializer,
-    injectedComponents: [],
+    // The app will load until this property is true
     isReady: false,
+    // The plugin's App container,
+    mainComponent: App,
+    name,
+    icon,
+    // pluginLogo	file	The plugin's logo
+    injectedComponents: [],
     isRequired: pluginPkg.strapi.required || false,
     layout: null,
     lifecycles,
-    mainComponent: App,
-    name,
+    // Whether or not display the plugin's blockerComponent instead of the main component
     preventComponentRendering: false,
+    // The plugin's translation files
     trads,
+    // reducers	object	The plugin's redux reducers
+
+    // Refer to the Plugins settings API
+    // https://strapi.io/documentation/developer-docs/latest/development/local-plugins-customization.html#plugin-s-front-end-settings-api
     settings: {
       menuSection: {
         id: 'Internalization',
         title: {
           id: pluginId,
-          defaultMessage: 'Custom Plugin'
+          defaultMessage: 'Custom Plugin Gayrat section'
         },
         links: [
           {
             title: {
               id: `${pluginId}.answer`,
-              defaultMessage: 'The Answer'
+              defaultMessage: 'The Answer тест'
+            },
+            to: `${strapi.settingsBaseURL}/${pluginId}/answer`,
+            Component: PostCount
+          },
+          {
+            title: {
+              id: `${pluginId}.question`,
+              defaultMessage: 'Интернализация вопрос'
             },
             to: `${strapi.settingsBaseURL}/${pluginId}/answer`,
             Component: PostCount
@@ -46,11 +66,15 @@ export default strapi => {
         ]
       }
     },
+    // Define where the link of your plugin will be set. Without this your plugin will not display a link in the left menu
     menu: {
-      pluginsSection: [
+
+
+      // Отвечает за меню соева в секции PLUGINS
+      pluginsSectionLinks: [
         {
           destination: `/plugins/${pluginId}`,
-          icon,
+          icon: 'paw',
           label: {
             id: `${pluginId}.plugin.name`,
             defaultMessage: name,
@@ -63,6 +87,13 @@ export default strapi => {
             //   subject: null,
             // },
           ],
+        },
+        {
+          destination: `/plugins/${pluginId}/2`,
+          icon: 'cat',
+          label: 'Тестовое меню слева',
+          name:name+1,
+          permissions: [],
         },
       ],
     },
